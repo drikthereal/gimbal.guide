f87b4209de71db8137efdfb031f7e2ad9617f142
drikthereal <drikthereal@users.noreply.github.com>
2017-11-05 13:31:55 +0000

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,19 @@
 var csvURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQmNnMRfkOUy3KGfHGaDJ7vBPG1UYVQiPWpxDKLlLCHyOsXd1PsEah_SHInbNx49D_UclLlVPgGFiZo/pub?gid=0&single=true&output=csv';
 
-function load() {
-    $.get(csvURL, function(csv) {
-        var rows = csv.split('\n');
-        $.each(rows, function(i, row) {
-            var cols = row.split(',');
-            console.info(cols);
-        });
+function getCsvRows(csv, callback) {
+    var rows = csv.split('\n');
+    $.each(rows, function(i, row) {
+        var cols = row.split(',');
+        callback(i, cols);
     });
 }
 
+function processCsvRow(rowId, cols) {
+    console.info(rowId, cols);
+}
+
 if (window.location.pathname === '/') {
-    load();
+    $.get(csvURL, function(csv) {
+        getCsvRows(csv, processCsvRow);
+    });
 }
