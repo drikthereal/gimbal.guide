8fca464941f8d142bff490012ab79588b49fb3d8
drikthereal <drikthereal@users.noreply.github.com>
2017-11-05 13:26:14 +0000

    Update and rename script.coffee to script.js

--- static/js/script.coffee
+++
@@ -1,3 +0,0 @@
----
----
-{% include _coffee/load.coffee %}
---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+var csvURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQmNnMRfkOUy3KGfHGaDJ7vBPG1UYVQiPWpxDKLlLCHyOsXd1PsEah_SHInbNx49D_UclLlVPgGFiZo/pub?gid=0&single=true&output=csv';
+
+function load() {
+    $.get(csvURL, function(csv) {
+        var rows = csv.split('\n');
+        $.each(rows, function(i, row) {
+            var cols = row.split(',');
+            console.info(cols);
+        });
+    });
+}
+
+if (window.location.pathname === '/') {
+    load();
+}
