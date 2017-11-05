26a55cd8271601e914cb06a00b0fbad3f60c095f
drikthereal <drikthereal@users.noreply.github.com>
2017-11-05 13:49:03 +0000

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,27 @@
 var csvURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQmNnMRfkOUy3KGfHGaDJ7vBPG1UYVQiPWpxDKLlLCHyOsXd1PsEah_SHInbNx49D_UclLlVPgGFiZo/pub?gid=0&single=true&output=csv';
 
 function getCsvRows(csv, callback) {
-    var rows = csv.split('\n');
+    var rows = csv.split('\n'), html = '<table>';
     $.each(rows, function(i, row) {
-        var cols = row.split(',');
-        callback(i, cols);
+        html += getRowHtml(row);
     });
+    html += '</table>';
+    callback(html);
 }
 
-function processCsvRow(rowId, cols) {
-    console.info(rowId, cols);
+function getRowHtml(row) {
+    var cols = row.split(','), html = '<tr>';
+    $.each(cols, function(i, col) {
+        html += '<td>' + col + '</td>';
+    });
+    html += '</tr>';
+    return html;
 }
 
 if (window.location.pathname === '/') {
     $.get(csvURL, function(csv) {
-        getCsvRows(csv, processCsvRow);
+        getCsvRows(csv, function(html) {
+            $('body').append(html);
+        });
     });
 }
