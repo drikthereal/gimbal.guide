86149fed5db27bc9f819e869afc4d7eb3e4ae6b1
drikthereal <drikthereal@users.noreply.github.com>
2017-11-06 06:09:42 +0000

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,14 @@
 var csvURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQmNnMRfkOUy3KGfHGaDJ7vBPG1UYVQiPWpxDKLlLCHyOsXd1PsEah_SHInbNx49D_UclLlVPgGFiZo/pub?gid=0&single=true&output=csv';
 
+function makeTable(tableData, tableCols) {
+    $('#data-table').tabulator({
+        data: tableData,
+        columns: tableCols
+    });
+}
+
 if (window.location.pathname === '/') {
-    var keys = [];
+    var keys = [], data = [];
     $.get(csvURL, function(csv) {
         var rows = csv.split('\n');
         $.each(rows, function(rowIdx, row) {
@@ -9,12 +16,17 @@ if (window.location.pathname === '/') {
             if (rowIdx === 0) {
                 keys = cols;
             } else {
-                var colObj = {};
+                var colObj = {}, tableCols = [];
                 $.each(cols, function(colIdx, col) {
-                    colObj[keys[colIdx]] = col;
+                    var key = keys[colIdx];
+                    colObj[key] = col;
+                    tableCols.push({title: key.charAt(0).toUpperCase() + key.slice(1), field: key});
                 });
-                console.info(rowIdx, colObj);
+                data.push(colObj);
             }
         });
+        if (data) {
+            makeTable(data, tableCols);
+        }
     });
 }
