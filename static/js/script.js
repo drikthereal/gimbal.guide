16eb073297c54053284a5a6abf9bf5be34f9715e
drikthereal <drikthereal@users.noreply.github.com>
2017-11-06 05:05:55 +0000

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,20 @@
 var csvURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQmNnMRfkOUy3KGfHGaDJ7vBPG1UYVQiPWpxDKLlLCHyOsXd1PsEah_SHInbNx49D_UclLlVPgGFiZo/pub?gid=0&single=true&output=csv';
 
-function getCsvRows(csv, callback) {
-    var rows = csv.split('\n'), html = '<table>';
-    $.each(rows, function(i, row) {
-        html += getRowHtml(row);
-    });
-    html += '</table>';
-    callback(html);
-}
-
-function getRowHtml(row) {
-    var cols = row.split(','), html = '<tr>';
-    $.each(cols, function(i, col) {
-        html += '<td>' + col + '</td>';
-    });
-    html += '</tr>';
-    return html;
-}
-
 if (window.location.pathname === '/') {
+    var keys = [];
     $.get(csvURL, function(csv) {
-        getCsvRows(csv, function(html) {
-            $('body').append(html);
+        var rows = csv.split('\n');
+        $.each(rows, function(rowIdx, row) {
+            var cols = row.split(',');
+            if (rowIdx === 0) {
+                keys = cols;
+            } else {
+                var colObj = {};
+                $.each(cols, function(colIdx, col) {
+                    colObj[keys[colIdx]] = col;
+                });
+            }
+            console.info(rowIdx, colObj);
         });
     });
 }
