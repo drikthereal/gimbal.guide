4b3a63425f7fba6fcdff44eabd2bd8bf7a72439e
drikthereal <drikthereal@users.noreply.github.com>
2017-11-12 19:43:19 +0000

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,9 @@
 var csvURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQmNnMRfkOUy3KGfHGaDJ7vBPG1UYVQiPWpxDKLlLCHyOsXd1PsEah_SHInbNx49D_UclLlVPgGFiZo/pub?gid=0&single=true&output=csv';
+var SPLIT_SEPARATOR = ';';
+
 var mutators = {
     compatibility: function(val) {
-        return val.split(';').join('<br>');
+        return val.split(SPLIT_SEPARATOR).join('<br>');
     }
 };
 var filters = {
@@ -39,7 +41,7 @@ if (window.location.pathname === '/') {
                     var key = keys[colIdx];
                     colObj[key] = col;
                     if (filterKeys.indexOf(key) > -1) {
-                        var newKeys = typeof mutators[key] === undefined ? [col] : mutators[key](col);
+                        var newKeys = col.split(SPLIT_SEPARATOR);
                         $.each(newKeys, function (newKeyIdx, newKey) {
                             filterOptions[key][newKey] = true;
                         });
